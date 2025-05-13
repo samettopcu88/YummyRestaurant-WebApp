d8ee36a4dbb884c92080062dd4def1f98cb88c6d
samettopcu88 <samet.topcu.88@icloud.com>
2025-05-13 05:09:58 +0300

    Entity'ler ve paketler hazırlandı.

---
+++ Yummy.WebApi/Entities/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace Yummy.WebApi.Entities
+{
+    public class Image
+    {
+        public int ImageId { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
