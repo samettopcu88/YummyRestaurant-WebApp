18812e04bddafe83add2e6168e232767163de965
samettopcu88 <samet.topcu.88@icloud.com>
2025-05-15 01:49:05 +0300

    AutoMapper ile mapping işlemleri yapıldı.

---
+++ Yummy.WebApi/Mapping/GeneralMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Yummy.WebApi.Dtos.FeatureDtos;
+using Yummy.WebApi.Dtos.MessageDtos;
+using Yummy.WebApi.Entities;
+
+namespace Yummy.WebApi.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Feature, ResultFeatureDto>().ReverseMap();
+            CreateMap<Feature, CreateFeatureDto>().ReverseMap();
+            CreateMap<Feature, UpdateFeatureDto>().ReverseMap();
+            CreateMap<Feature, GetByIdFeatureDto>().ReverseMap();
+
+            CreateMap<Message, ResultMessageDto>().ReverseMap();
+            CreateMap<Message, CreateMessageDto>().ReverseMap();
+            CreateMap<Message, UpdateMessageDto>().ReverseMap();
+            CreateMap<Message, GetByIdMessageDto>().ReverseMap();
+        }
+    }
+}
