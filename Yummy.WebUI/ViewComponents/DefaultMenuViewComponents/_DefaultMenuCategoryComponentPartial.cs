d555e41c0a216b9cfcd4a48424b07fc22052c134
samettopcu88 <samet.topcu.88@icloud.com>
2025-06-29 20:54:13 +0300

    menu partials done

---
+++ Yummy.WebUI/ViewComponents/DefaultMenuViewComponents/_DefaultMenuCategoryComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Yummy.WebUI.Dtos.CategoryDtos;
+
+namespace Yummy.WebUI.ViewComponents.DefaultMenuViewComponents
+{
+    public class _DefaultMenuCategoryComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultMenuCategoryComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7136/api/Categories/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ Yummy.WebUI/ViewComponents/DefaultMenuViewComponents/_DefaultMenuViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Yummy.WebUI.ViewComponents.DefaultMenuViewComponents
+{
+    public class _DefaultMenuViewComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
