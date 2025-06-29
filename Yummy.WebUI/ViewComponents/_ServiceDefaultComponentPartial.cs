dcab07910cb6533ccba1c4fa232674bb6c80b559
samettopcu88 <samet.topcu.88@icloud.com>
2025-06-29 14:22:57 +0300

    Service consume

--- Yummy.WebUI/ViewComponents/_ServiceDefaultComponentPartial.cs
+++ Yummy.WebUI/ViewComponents/_ServiceDefaultComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Yummy.WebUI.Dtos.ServiceDtos;
 
 namespace Yummy.WebUI.ViewComponents
 {
     public class _ServiceDefaultComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private IHttpClientFactory _httpClientFactory;
+
+        public _ServiceDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7136/api/Services");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
