c08c9809faafc1f4d3c351ea8f425ae33913f22e
samettopcu88 <samet.topcu.88@icloud.com>
2025-07-02 15:44:49 +0300

    chef and footer sections are done

---
+++ Yummy.WebUI/ViewComponents/_ChefDefualtComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Yummy.WebUI.Dtos.ChefDtos;
+
+namespace Yummy.WebUI.ViewComponents
+{
+    public class _ChefDefualtComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ChefDefualtComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7136/api/Chefs/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultChefDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
