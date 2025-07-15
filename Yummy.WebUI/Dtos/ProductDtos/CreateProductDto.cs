4b7580e7a43468d01dd7f178e4005bb61d3967ec
samettopcu88 <samet.topcu.88@icloud.com>
2025-07-16 02:51:56 +0300

    admin panel product process: ProductController, Product Dtos, and Views

---
+++ Yummy.WebUI/Dtos/ProductDtos/CreateProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Yummy.WebUI.Dtos.ProductDtos
+{
+    public class CreateProductDto
+    {
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
